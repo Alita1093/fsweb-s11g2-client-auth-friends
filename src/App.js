61daa79e01fc20608334888fd52b7b55b4e8b12c
Alita1093 <furkanozturk1635@gmail.com>
2023-02-08 01:08:56 +0300

    s11-g2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import Headers from "./components/header";
+import Login from "./components/login";
+import { Route } from "react-router-dom";
+import AddFriend from "./components/addFriend";
+import PrivateRoute from "./components/PrivateRoute";
+import FriendList from "./components/friendList";
 
 function App() {
+  console.log(localStorage.getItem("token"));
   return (
     <div className="App">
-      <h1>Client Auth Projesi: Friends</h1>
+      <Route path="/" component={Headers} />
+      <Route path="/api/login" component={Login} />
+      <Route path="/api/friends" component={AddFriend} />
+      <PrivateRoute path="/api/friendsList" component={FriendList} />
     </div>
   );
 }
